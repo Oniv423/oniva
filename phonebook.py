286fe6b38c5892f08306242a0d8a42748ededfa2
Oniv423 <mohaiminamorshed@gmail.com>
2026-06-20 13:45:48 +0600

    Add files via upload

---
+++ phonebook.py
@@ -0,0 +1,7 @@
+import sys
+def intial_phonebook():
+    rows, cols = int(input("Please enter initial number of contacts:")), 5
+    phone_book =[]
+    print(phone_book)
+    for i in range(rows):
+        print("\nEnter contact %d details")
