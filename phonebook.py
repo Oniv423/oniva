7d26e7be8d367521002b3984a1803b8cabd26d1d
Oniv423 <mohaiminamorshed@gmail.com>
2026-06-26 13:45:45 +0600

    Add files via upload

--- phonebook.py
+++ phonebook.py
@@ -5,3 +5,30 @@ def intial_phonebook():
     print(phone_book)
     for i in range(rows):
         print("\nEnter contact %d details")
+        print("\nEnter contact %d details in the following order (ONLY):" % (i+1))
+        print("NOTE: * indicates mandatory fields")
+        print("...................................................................................")
+        temp = []
+        for j in range (cols):
+            if j == 0:
+                temp.append(str(input("Enter name*:  ")))
+                if temp [j] == '' or temp [j] == ' ':
+                    sys.exit("Name is a mandatory field")
+                    if j == 1 :
+                        temp.append(int(input("Enter number*:  ")))
+                    if j == 2 :
+                        temp.append(int(input("Enter e-mail address*:  ")))
+                        if temp [j] == '' or temp [j] == ' ':
+                            temp [j] = None
+                    if j == 3 :
+                        temp.append(int(input("Enter date of birth*:  ")))
+                        if temp[j] == '' or temp[j] == ' ':
+                         temp[j] = None
+                         if j == 4:
+                          temp.append(
+                        str(input("Enter category(Family/Friends/Work/Others): ")))
+                        if temp[j] == "" or temp[j] == ' ':
+                         temp[j] = None
+        phone_book.append(temp)
+    print(phone_book)
+    return phone_book
